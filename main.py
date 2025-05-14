54f80bc90f3b301fa93a5fbfdc53fc592dfee52c
Shoedonym <disasta121@hotmail.com>
2025-05-14 19:13:59 -0400

    Added player character with hitbox and rotation

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 import pygame
 from constants import SCREEN_WIDTH, SCREEN_HEIGHT, ASTEROID_MIN_RADIUS, ASTEROID_KINDS, ASTEROID_SPAWN_RATE, ASTEROID_MAX_RADIUS
+from player import Player
 
 def main():
     pygame.init()
@@ -8,14 +9,20 @@ def main():
     print(f"Screen height: {SCREEN_HEIGHT}")
     
     screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
+    clock = pygame.time.Clock()
+    dt = 0
+    player_1 = Player(SCREEN_WIDTH / 2, SCREEN_HEIGHT / 2)
     
     #the core game loop
     while True:
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 return
-        screen.fill
-        pygame.display.flip()  
+        screen.fill("black")
+        player_1.update(dt)
+        player_1.draw(screen)
+        pygame.display.flip()
+        dt = clock.tick(60) / 1000
 
 if __name__ == "__main__":
     main()
