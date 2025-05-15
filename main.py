2a56136c511882536862c33a4d3891f61343971e
Shoedonym <disasta121@hotmail.com>
2025-05-15 10:48:22 -0400

    Added movement, asteroids, groups, and collision

--- main.py
+++ main.py
@@ -1,6 +1,9 @@
 import pygame
+import sys
 from constants import SCREEN_WIDTH, SCREEN_HEIGHT, ASTEROID_MIN_RADIUS, ASTEROID_KINDS, ASTEROID_SPAWN_RATE, ASTEROID_MAX_RADIUS
 from player import Player
+from asteroid import Asteroid
+from asteroidfield import AsteroidField
 
 def main():
     pygame.init()
@@ -11,7 +14,14 @@ def main():
     screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
     clock = pygame.time.Clock()
     dt = 0
+    updatable = pygame.sprite.Group()
+    drawable = pygame.sprite.Group()
+    asteroids = pygame.sprite.Group()
+    Player.containers = (updatable, drawable)
+    Asteroid.containers = (asteroids, updatable, drawable)
+    AsteroidField.containers = (updatable)
     player_1 = Player(SCREEN_WIDTH / 2, SCREEN_HEIGHT / 2)
+    outfield = AsteroidField()
     
     #the core game loop
     while True:
@@ -19,8 +29,13 @@ def main():
             if event.type == pygame.QUIT:
                 return
         screen.fill("black")
-        player_1.update(dt)
-        player_1.draw(screen)
+        updatable.update(dt)
+        for spacerock in asteroids:
+            if spacerock.collision_check(player_1):
+                print("Game over!")
+                sys.exit()
+        for drawing in drawable:
+            drawing.draw(screen)
         pygame.display.flip()
         dt = clock.tick(60) / 1000
 
